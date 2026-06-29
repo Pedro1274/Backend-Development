febffb28916ce0634600ceff7a3eb5fa855f19a7
pedro1274 <pedroruan17@hotmail.com>
2026-06-29 11:40:40 -0300

    Atualizações de code style

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI  # ty:ignore[unresolved-import]
 
 app = FastAPI()
 
+
 @app.get('/')
 def read_root():
-    return{'message': 'Hello, world!'}
+    return {'message': 'Hello, world!'}
