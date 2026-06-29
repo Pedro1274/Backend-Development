5a444a82881854a18b63500746bff4d7e3fb09c7
pedro1274 <pedroruan17@hotmail.com>
2026-06-29 12:19:40 -0300

    Limpeza de arquivos

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from backend.app import app
@@ -9,3 +11,4 @@ def test_root_deve_retornar_ola_mundo():
     response = client.get('/')
 
     assert response.json() == {'message': 'Hello, world!'}
+    assert response.status_code == HTTPStatus.OK
