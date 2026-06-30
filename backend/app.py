1f079c17cbf33d2075a929cb39394cec90ee7495
pedro1274 <pedroruan17@hotmail.com>
2026-06-30 12:58:38 -0300

    Nova rota HTML e códigos de status

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI  # ty:ignore[unresolved-import]
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200)
 def read_root():
     return {'message': 'Hello, world!'}
+
+@app.get('/pretty_root', status_code=200, response_class=HTMLResponse)
+def read_pretty_root():
+    return """
+    <html>
+        <head>
+            <title>Our new Hello world!</title>
+        </head>
+        <body>
+            <h1>Hello, world!</h1>
+        </body>
+    </html>"""
