e20ab6b004d41ed8ce5cf2480c13bc2a342a95db
pedro1274 <pedroruan17@hotmail.com>
2026-07-03 14:02:56 -0300

    Rotas de teste e delete, testes incluídos para ambas

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI  # ty:ignore[unresolved-import]
+from fastapi import FastAPI, HTTPException  # ty:ignore[unresolved-import]
 from fastapi.responses import HTMLResponse
 
 from backend.schemas import Message, UserDB, UserList, UserPublic, UserSchema
@@ -27,10 +27,7 @@ def read_pretty_root():
 
 @app.post('/users/', status_code=201, response_model=UserPublic)
 def create_user(user: UserSchema):
-    user_with_id = UserDB(
-        **user.model_dump(),
-        id=len(database) + 1,
-    )
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
 
     database.append(user_with_id)
 
@@ -40,3 +37,23 @@ def create_user(user: UserSchema):
 @app.get('/users/', status_code=200, response_model=UserList)
 def get_users():
     return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database))
+
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(status_code=404, detail='Usuário não encontrado!')
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=UserPublic)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(status_code=404, detail='Usuário não encontrado!')
+
+    return database.pop(user_id - 1)
--- tests/test_app.py
+++ tests/test_app.py
@@ -49,3 +49,32 @@ def test_read_users(client):
     assert response.json() == {
         'users': [{'id': 1, 'username': 'alice', 'email': 'alice@example.com'}]
     }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': '1234',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
