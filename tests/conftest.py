2726491833d155570356e7123e1e93d869801115
pedro1274 <pedroruan17@hotmail.com>
2026-07-04 21:03:02 -0300

    Melhoria de validação do teste de criação do usuário com dicionário

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
 
 from backend.app import app
@@ -22,3 +25,21 @@ def session():
         yield session
 
     table_registry.metadata.drop_all(engine)
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2026, 7, 4)):
+    def fake_time_handler(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+
+    event.listen(model, 'before_insert', fake_time_handler)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_handler)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,14 +1,24 @@
+from dataclasses import asdict
+from datetime import datetime
+
 from sqlalchemy import select
 
 from backend.models import User
 
 
-def test_create_user(session):
-    user = User(username='teste', email='teste@teste.com', password='1234')
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User, time=datetime.now()) as time:
+        user = User(username='teste', email='teste@teste.com', password='1234')
 
-    session.add(user)
-    session.commit()
+        session.add(user)
+        session.commit()
 
     user = session.scalar(select(User).where(User.password == '1234'))
 
-    assert user.password == '1234'
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'teste',
+        'email': 'teste@teste.com',
+        'password': '1234',
+        'created_at': time,
+    }
